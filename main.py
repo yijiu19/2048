7acc88f0457a917ca18526e554c2c59f007379df
yijiu19 <109260556+yijiu19@users.noreply.github.com>
2023-01-29 21:15:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,120 @@
-import cv2
-
-# 加载摄像头
-cap = cv2.VideoCapture(0)
-# 设置背景帧
-_, first_frame = cap.read()
-previous_gray = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)
-# 初始化检测点
-previous_points = cv2.goodFeaturesToTrack(previous_gray, maxCorners=10, qualityLevel=0.01, minDistance=30, blockSize=3)
-
-# 循环检测
-while True:
-    # 读取摄像头
-    _, frame = cap.read()
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # 计算光流
-    new_points, status, error = cv2.calcOpticalFlowPyrLK(previous_gray, gray_frame, previous_points, None)
-
-    # 得到移动点
-    x_movement, y_movement = 0, 0
-    for i, (new, old) in enumerate(zip(new_points, previous_points)):
-        a, b = new.ravel()
-        c, d = old.ravel()
-        x_movement += (a-c)
-        y_movement += (b-d)
-    if x_movement > 35:
-        print('右移')
-        break
-    elif x_movement < -35:
-        print('左移')
-        break
+from tkinter import Frame, Label, CENTER
+import random
+import logic
+import constants as c
+import cv
 
 
-    # 更新前一帧
-    previous_gray = gray_frame.copy()
-    previous_points = new_points.reshape(-1, 1, 2)
+def gen():  # 定义一个名为gen的函数，用于生成随机数
+    return random.randint(0, c.GRID_LEN - 1)
 
-# 释放摄像头
-cap.release()
-cv2.destroyAllWindows()
-####
+
+class GameGrid(Frame):  # 定义一个名为GameGrid的类，继承自tkinter的Frame类
+    def __init__(self):     # 定义GameGrid类的构造函数
+        Frame.__init__(self)
+
+        self.grid()
+        self.master.title('2048')
+        self.master.bind("<Key>", self.key_down)  # 绑定函数
+        self.master.bind("<Key>", cv.cvview)
+
+        # 定义一个字典，用于存储不同按键对应的操作a
+        self.commands = {
+            c.KEY_UP: logic.up,
+            c.KEY_DOWN: logic.down,
+            c.KEY_LEFT: logic.left,
+            c.KEY_RIGHT: logic.right,
+            c.KEY_UP_ALT1: logic.up,
+            c.KEY_DOWN_ALT1: logic.down,
+            c.KEY_LEFT_ALT1: logic.left,
+            c.KEY_RIGHT_ALT1: logic.right,
+            c.KEY_UP_ALT2: logic.up,
+            c.KEY_DOWN_ALT2: logic.down,
+            c.KEY_LEFT_ALT2: logic.left,
+            c.KEY_RIGHT_ALT2: logic.right,
+        }
+
+        self.grid_cells = []    # 定义一个空列表，用于存储游戏界面中的每一个格子
+        self.init_grid()        # 调用函数init_grid，用于初始化游戏界面
+        self.matrix = logic.new_game(c.GRID_LEN)    # 创建一个新的游戏界面
+        self.history_matrixs = []
+        self.update_grid_cells()    # 更新游戏界面
+
+        self.mainloop()
+
+    def init_grid(self):    # 定义一个函数，用于初始化游戏界面
+        background = Frame(self, bg=c.BACKGROUND_COLOR_GAME, width=c.SIZE, height=c.SIZE)
+        background.grid()
+
+        for i in range(c.GRID_LEN):
+            grid_row = []
+            for j in range(c.GRID_LEN):
+                cell = Frame(
+                    background,
+                    bg=c.BACKGROUND_COLOR_CELL_EMPTY,
+                    width=c.SIZE / c.GRID_LEN,
+                    height=c.SIZE / c.GRID_LEN
+                )
+                cell.grid(
+                    row=i,
+                    column=j,
+                    padx=c.GRID_PADDING,
+                    pady=c.GRID_PADDING
+                )
+                t = Label(
+                    master=cell,
+                    text="",
+                    bg=c.BACKGROUND_COLOR_CELL_EMPTY,
+                    justify=CENTER,
+                    font=c.FONT,
+                    width=5,
+                    height=2)
+                t.grid()
+                grid_row.append(t)
+            self.grid_cells.append(grid_row)
+
+    def update_grid_cells(self):    # 定义一个函数，用于更新游戏界面
+        for i in range(c.GRID_LEN):
+            for j in range(c.GRID_LEN):
+                new_number = self.matrix[i][j]
+                if new_number == 0:
+                    self.grid_cells[i][j].configure(text="", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
+                else:
+                    self.grid_cells[i][j].configure(
+                        text=str(new_number),
+                        bg=c.BACKGROUND_COLOR_DICT[new_number],
+                        fg=c.CELL_COLOR_DICT[new_number]
+                    )
+        self.update_idletasks()
+
+    def key_down(self, event):  # 定义一个函数，用于处理按键事件
+        key = event.keysym
+        print(event)
+        if key == c.KEY_QUIT:
+            exit()
+        if key == c.KEY_BACK and len(self.history_matrixs) > 1:
+            self.matrix = self.history_matrixs.pop()
+            self.update_grid_cells()
+            print('back on step total step:', len(self.history_matrixs))
+        elif key in self.commands:
+            self.matrix, done = self.commands[key](self.matrix)
+            if done:
+                self.matrix = logic.add_two(self.matrix)
+                # record last move
+                self.history_matrixs.append(self.matrix)
+                self.update_grid_cells()
+                if logic.game_state(self.matrix) == 'win':
+                    self.grid_cells[1][1].configure(text="You", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
+                    self.grid_cells[1][2].configure(text="Win!", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
+                if logic.game_state(self.matrix) == 'lose':
+                    self.grid_cells[1][1].configure(text="You", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
+                    self.grid_cells[1][2].configure(text="Lose!", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
+
+    def generate_next(self):    # 定义一个函数，用于随机生成下一个数字
+        index = (gen(), gen())
+        while self.matrix[index[0]][index[1]] != 0:
+            index = (gen(), gen())
+        self.matrix[index[0]][index[1]] = 2
+
+
+game_grid = GameGrid()
