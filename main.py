be21c56f6f28a531c00d3b6e314869c5f402b119
yijiu19 <109260556+yijiu19@users.noreply.github.com>
2023-02-04 17:13:34 +0800

    Create main.py

--- main.py
+++ main.py
@@ -1,120 +1,105 @@
-from tkinter import Frame, Label, CENTER
+import copy
 import random
-import logic
-import constants as c
 import cv
 
-
-def gen():  # 定义一个名为gen的函数，用于生成随机数
-    return random.randint(0, c.GRID_LEN - 1)
+view = [[0] * 4 for i in range(4)]
 
 
-class GameGrid(Frame):  # 定义一个名为GameGrid的类，继承自tkinter的Frame类
-    def __init__(self):     # 定义GameGrid类的构造函数
-        Frame.__init__(self)
-
-        self.grid()
-        self.master.title('2048')
-        self.master.bind("<Key>", self.key_down)  # 绑定函数
-        self.master.bind("<Key>", cv.cvview)
-
-        # 定义一个字典，用于存储不同按键对应的操作a
-        self.commands = {
-            c.KEY_UP: logic.up,
-            c.KEY_DOWN: logic.down,
-            c.KEY_LEFT: logic.left,
-            c.KEY_RIGHT: logic.right,
-            c.KEY_UP_ALT1: logic.up,
-            c.KEY_DOWN_ALT1: logic.down,
-            c.KEY_LEFT_ALT1: logic.left,
-            c.KEY_RIGHT_ALT1: logic.right,
-            c.KEY_UP_ALT2: logic.up,
-            c.KEY_DOWN_ALT2: logic.down,
-            c.KEY_LEFT_ALT2: logic.left,
-            c.KEY_RIGHT_ALT2: logic.right,
-        }
-
-        self.grid_cells = []    # 定义一个空列表，用于存储游戏界面中的每一个格子
-        self.init_grid()        # 调用函数init_grid，用于初始化游戏界面
-        self.matrix = logic.new_game(c.GRID_LEN)    # 创建一个新的游戏界面
-        self.history_matrixs = []
-        self.update_grid_cells()    # 更新游戏界面
-
-        self.mainloop()
-
-    def init_grid(self):    # 定义一个函数，用于初始化游戏界面
-        background = Frame(self, bg=c.BACKGROUND_COLOR_GAME, width=c.SIZE, height=c.SIZE)
-        background.grid()
-
-        for i in range(c.GRID_LEN):
-            grid_row = []
-            for j in range(c.GRID_LEN):
-                cell = Frame(
-                    background,
-                    bg=c.BACKGROUND_COLOR_CELL_EMPTY,
-                    width=c.SIZE / c.GRID_LEN,
-                    height=c.SIZE / c.GRID_LEN
-                )
-                cell.grid(
-                    row=i,
-                    column=j,
-                    padx=c.GRID_PADDING,
-                    pady=c.GRID_PADDING
-                )
-                t = Label(
-                    master=cell,
-                    text="",
-                    bg=c.BACKGROUND_COLOR_CELL_EMPTY,
-                    justify=CENTER,
-                    font=c.FONT,
-                    width=5,
-                    height=2)
-                t.grid()
-                grid_row.append(t)
-            self.grid_cells.append(grid_row)
-
-    def update_grid_cells(self):    # 定义一个函数，用于更新游戏界面
-        for i in range(c.GRID_LEN):
-            for j in range(c.GRID_LEN):
-                new_number = self.matrix[i][j]
-                if new_number == 0:
-                    self.grid_cells[i][j].configure(text="", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
-                else:
-                    self.grid_cells[i][j].configure(
-                        text=str(new_number),
-                        bg=c.BACKGROUND_COLOR_DICT[new_number],
-                        fg=c.CELL_COLOR_DICT[new_number]
-                    )
-        self.update_idletasks()
-
-    def key_down(self, event):  # 定义一个函数，用于处理按键事件
-        key = event.keysym
-        print(event)
-        if key == c.KEY_QUIT:
-            exit()
-        if key == c.KEY_BACK and len(self.history_matrixs) > 1:
-            self.matrix = self.history_matrixs.pop()
-            self.update_grid_cells()
-            print('back on step total step:', len(self.history_matrixs))
-        elif key in self.commands:
-            self.matrix, done = self.commands[key](self.matrix)
-            if done:
-                self.matrix = logic.add_two(self.matrix)
-                # record last move
-                self.history_matrixs.append(self.matrix)
-                self.update_grid_cells()
-                if logic.game_state(self.matrix) == 'win':
-                    self.grid_cells[1][1].configure(text="You", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
-                    self.grid_cells[1][2].configure(text="Win!", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
-                if logic.game_state(self.matrix) == 'lose':
-                    self.grid_cells[1][1].configure(text="You", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
-                    self.grid_cells[1][2].configure(text="Lose!", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
-
-    def generate_next(self):    # 定义一个函数，用于随机生成下一个数字
-        index = (gen(), gen())
-        while self.matrix[index[0]][index[1]] != 0:
-            index = (gen(), gen())
-        self.matrix[index[0]][index[1]] = 2
+def form_view(r: str):
+    if r == 'w':
+        return [[view[x][i] for x in range(len(view[i]))] for i in range(len(view))]
+    if r == 'a':
+        return copy.deepcopy(view)
+    if r == 's':
+        return [[view[x][i] for x in range(len(view[i]) - 1, -1, -1)] for i in range(len(view))]
+    return [item[::-1] for item in view]
 
 
-game_grid = GameGrid()
+def to_view(r: str, v):
+    if r == 'w':
+        return [[v[x][i] for x in range(len(v[i]))] for i in range(len(v))]
+    if r == 'a':
+        return copy.deepcopy(v)
+    if r == 's':
+        return [[v[x][i] for x in range(len(v[i]))] for i in range(len(v) - 1, -1, -1)]
+    return [item[::-1] for item in v]
+
+
+def zero_to_end(item):
+    return [x for x in item if x] + [0] * item.count(0)
+
+
+def can_merge(v):
+    for x in v:
+        item = zero_to_end(x)
+        if x != item:
+            return True
+        for i in range(len(item) - 1):
+            if item[i] and item[i] == item[i + 1]:
+                return True
+    return False
+
+
+def show_view():
+    print('-' * 29)
+    for v in view:
+        print('|', end='')
+        for x in v:
+            if x:
+                print(' %4d |' % x, end='')
+            else:
+                print(' ' * 6 + '|', end='')
+        print()
+        print('-' * 29)
+    print()
+
+
+def add_new_item():
+    can_add_row = [i for i in view if 0 in i]
+    target_row = can_add_row[random.randint(0, len(can_add_row) - 1)]
+    can_add_index = [i for i in range(len(target_row)) if not target_row[i]]
+    target_index = can_add_index[random.randint(0, len(can_add_index) - 1)]
+    target_row[target_index] = random.randint(1, 2) * 2
+
+
+def merge(v, r: str):
+    for x in v:
+        x[:] = zero_to_end(x)
+        for i in range(len(x) - 1):
+            if x[i] and x[i] == x[i + 1]:
+                x[i] = x[i] * 2
+                x[i + 1] = 0
+        x[:] = zero_to_end(x)
+    global view
+    view = to_view(r, v)
+    add_new_item()
+    if not game_over():
+        play()
+
+
+def game_over():
+    if can_merge(form_view('w')) or can_merge(form_view('a')) or can_merge(form_view('s')) or can_merge(form_view('d')):
+        return False
+    show_view()
+    print('游戏结束')
+    return True
+
+
+def play():
+    show_view()
+    # ipt = input()
+    ipt = cv.cvview()
+
+    if ipt not in ('w', 'a', 's', 'd'):
+        print('输入有误')
+        play()
+    else:
+        v = form_view(ipt)
+        if can_merge(v):
+            return merge(v, ipt)
+        print('操作失败')
+        play()
+
+
+add_new_item()
+play()
